2fb78c728420b5d27ecc2560e877d7ef3e1cfbca
ZVFrozel <zvfrozel@gmail.com>
2025-02-03 23:10:44 +0530

    Add forbidden template

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vll = vector<long long>;
+using vvll = vector<vector<long long>>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vb = vector<bool>;
+using vvb = vector<vector<bool>>;
+using pii = pair<int, int>;
+using vpii = vector<pair<int, int>>;
+
+void go() {}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    go();
+    return 0;
+}
+
+// Ideally don't copy template each time but memorize relevant parts for contest
